2a8c8bf8bf900c54946dae14ad63a80e42b2ca62
SexySteak <https://gitee.com/sexysteak>
2023-01-01 09:51:02 +0800

    modified:   README.md 	new file:   backEnd/.gitignore 	new file:   backEnd/.mvn/wrapper/maven-wrapper.jar 	new file:   backEnd/.mvn/wrapper/maven-wrapper.properties 	new file:   backEnd/mvnw 	new file:   backEnd/mvnw.cmd 	new file:   backEnd/pom.xml 	new file:   backEnd/src/main/java/com/example/keshe/KeSheApplication.java 	new file:   backEnd/src/main/java/com/example/keshe/controller/UserController.java 	new file:   backEnd/src/main/java/com/example/keshe/entity/User.java 	new file:   backEnd/src/main/java/com/example/keshe/mapper/UserMapper.java 	new file:   backEnd/src/main/resources/application.yaml 	new file:   backEnd/src/test/java/com/example/keshe/KeSheApplicationTests.java 	new file:   frontEnd/.eslintrc.cjs 	new file:   frontEnd/.gitignore 	new file:   frontEnd/.prettierrc.json 	new file:   frontEnd/.vscode/extensions.json 	new file:   frontEnd/README.md 	new file:   frontEnd/index.html 	new file:   frontEnd/package.json 	new file:   frontEnd/public/favicon.ico 	new file:   frontEnd/src/App.vue 	new file:   frontEnd/src/assets/base.css 	new file:   frontEnd/src/assets/logo.svg 	new file:   frontEnd/src/assets/main.css 	new file:   frontEnd/src/components/HelloWorld.vue 	new file:   frontEnd/src/components/TheWelcome.vue 	new file:   frontEnd/src/components/WelcomeItem.vue 	new file:   frontEnd/src/components/icons/IconCommunity.vue 	new file:   frontEnd/src/components/icons/IconDocumentation.vue 	new file:   frontEnd/src/components/icons/IconEcosystem.vue 	new file:   frontEnd/src/components/icons/IconSupport.vue 	new file:   frontEnd/src/components/icons/IconTooling.vue 	new file:   frontEnd/src/main.js 	new file:   frontEnd/src/router/index.js 	new file:   frontEnd/src/views/AboutView.vue 	new file:   frontEnd/src/views/HomeView.vue 	new file:   frontEnd/vite.config.js

---
+++ backEnd/src/main/java/com/example/keshe/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.keshe.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.keshe.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
